91dcacd7008df42ba6e384c096d8caba6fb433d1
Burhan, Muhammad (DE - Duesseldorf) <mburhan@deloitte.de>
2019-03-21 11:00:57 +0100

    feature 1

--- library/src/main/java/com/local/library/Application.java
+++ library/src/main/java/com/local/library/Application.java
@@ -3,8 +3,5 @@ package com.local.library;
 public class Application {
     public static void main(String[] args) {
         System.out.print("Hello world - Feature 1");
-        System.out.print("Hello world - Feature 2");
-        System.out.print("Hello world - Feature 2a");
-        System.out.print("Hello world - Feature 2c");
     }
 }
