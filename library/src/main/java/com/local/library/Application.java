d8830e7ffd3622062999cfac0a271dd6ce8fed5b
Burhan, Muhammad (DE - Duesseldorf) <mburhan@deloitte.de>
2019-03-21 21:43:29 +0100

    no message

--- library/src/main/java/com/local/library/Application.java
+++ library/src/main/java/com/local/library/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.print("Hello world - Feature 1");
         System.out.print("Hello world - Feature 1a");
+        System.out.print("Hello world - Feature 1a snapshot");
     }
 }
