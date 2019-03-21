775598ce53bee40d7092b46f8af4051c5baad94d
Burhan, Muhammad (DE - Duesseldorf) <mburhan@deloitte.de>
2019-03-21 10:32:19 +0100

    initial commit

---
+++ library/src/main/java/com/local/library/Application.java
@@ -0,0 +1,10 @@
+package com.local.library;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.print("Hello world - Feature 1");
+        System.out.print("Hello world - Feature 2");
+        System.out.print("Hello world - Feature 2a");
+        System.out.print("Hello world - Feature 2c");
+    }
+}
