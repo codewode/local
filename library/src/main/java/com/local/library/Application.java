9ebf794ad235a9f244e013320450fe6ea23c83e6
Burhan, Muhammad (DE - Duesseldorf) <mburhan@deloitte.de>
2019-03-25 10:36:37 +0100

    bug

--- library/src/main/java/com/local/library/Application.java
+++ library/src/main/java/com/local/library/Application.java
@@ -3,6 +3,6 @@ package com.local.library;
 public class Application {
     public static void main(String[] args) {
         System.out.print("Hello world - Feature 1");
-        System.out.print("Hello world - Feature 1a");
+        System.out.print("Hello world - Feature bug 1a");
     }
 }
